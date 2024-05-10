73c42fd5ea5f72dfb3f7fca498e9de13b92a2115
Jordan Penn <k23067841@kcl.ac.uk>
2024-05-10 14:20:01 +0100

    Test commit 2

--- Simulations heteroskedastic.R
+++ Simulations heteroskedastic.R
@@ -1,6 +1,25 @@
+# Simulated data with preprocessing step for Phi(X)
 
-function budgetIV (
-  
-) {
-  
-}
+#preprocess_X <- function(X_dat, model){
+
+#  dim_X <- 
+
+#}
+
+#
+# Generate model with randomised parameters satisfying constraints:
+#
+# X = f_x (Z_1, Z_2, e_x)
+#
+# Y = theta * X + g_y(Z_1, Z_2, e_m, e_a)
+#
+# g_y(Z_1, Z_2, e_m, e_a) = e_m * Z_1 + e_a 
+#
+# Cov(Z_1, g_y) = -2 (high)
+#
+# Cov(Z_2, g_y) = 0.1 (low)
+#
+# theta = 1
+#
+# Cov()
+#
