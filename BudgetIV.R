576eabcd7a0372c32eea35cf8498ed61133911ee
Jordan Penn <k23067841@kcl.ac.uk>
2024-10-05 16:21:48 +0100

    BudgetIV update

--- BudgetIV.R
+++ BudgetIV.R
@@ -8,27 +8,42 @@
 #   (ii) Background budget constraints, parameterised by (tau_i, m_i)_{i=1}^K, also written (tau_vec, m_vec)
 
 library(MASS)
+library(lpSolve)
+
+# install.packages("Rglpk")
+library(Rglpk)
+
+set.seed(123)
 
 # install.packages("arrangements")
 library(arrangements)
 
 BudgetIV <- function(
-    beta_y, # Cross covariance vector Cov(Y, Z_vec)
+    beta_y, # Cross covariance vector Cov(Y, Z_vec), or point estimate thereof. A row vector or d_Z * 1 matrix.
     beta_phi, # Cross covariance vector Cov(Phi(X), Z_vec)
-    tau_vec, # Degrees of violation of (AWE') (see manuscript). Ordered set.
-    m_vec, # Ordered (increasing) set, demanding \sum_{i \in [d_Z]} { II (cov(Z_i, g_y) <= tau_i) } >= m_i, 
+    phi_basis, # The actual functions Phi(x), input as a list of expressions
+    tau_vec, # Degrees of violation of (AWE') (see manuscript). Ordered set
+    m_vec, # Ordered (increasing) set, demanding \sum_{i \in [d_Z]} { II (cov(Z_i, g_y) <= tau_i) } >= m_i 
            # where II is the indicator function 
-    tol=1e-10, # Tolerance for "being along a basis vector". Default set to 1e-10, 
-              # but choice of units for (X,Y,Z) affects the decision rule. 
-    dummy_infinity=1e10, # Dummy value for tau_{K+1} := infinity. Adjust if necessary. 
-    dom_ATE, # Domain of X to optimise ATE within (SET TO A )
-    ATE_grid_search_size=100
+    search_dom_ATE, # Domain of X to optimise ATE within (must be specified)
+    search_res_ATE=100, # How many values of X to optimite ATE in for each component of X (set to 100 in every component)
+    tol=1e-10, # Tolerance for "being along a basis vector". Default set to 1e-10
+    # but choice of units for (X,Y,Z) affects the decision rule. 
+    dummy_infinity=1e10, # Dummy value for tau_{K+1} := infinity. Adjust if necessary
+    delta_beta_y=NA # Half-width 
 ) {
   
   # print(length(beta_y))
   # print(ncol(beta_phi))
   
+  d_Z <- ncol(beta_y)
   
+  if (all(is.na(delta_beta_y))){delta_beta_y <- numeric(d_Z)}
+  
+  # if (search_res_ATE)
+  
+  # print(beta_phi)
+    
   # Warnings
   if (is.unsorted(tau_vec)) {
     stop('Please input tau constraints in increasing order.')
@@ -39,12 +54,21 @@ BudgetIV <- function(
   else if (is.unsorted(m_vec) || any(duplicated(m_vec))) {
     stop('The vector m must be strictly increasing, please see the definition of boldface m in the manuscript.')
   }
-  else if (length(beta_y) != nrow(beta_phi)) {
+  else if (length(beta_y) != ncol(beta_phi)) {
     stop('Cov(Y, Z) and Cov(Phi(X), Z) must be vectors of the same length for scalar Phi(X). Please call "BudgetIV" for treatment of vector Phi(X).')
   }
-  else if (length(beta_y) < ncol(beta_phi)){
+  else if (length(beta_y) < nrow(beta_phi)){
     stop('BudgetIV only supports partial identification in the regime d_{Phi} <= d_{Z}')
   }
+  else if (length(beta_y) != length(delta_beta_y)){
+    stop('If specifying half-width errors delta_beta_y, there must be as many errors as components of beta_y. Run get_covariance with confidence_threshold set to sum numeric in (0,1) to compute these')
+  }
+  else if (!all(delta_beta_y >= 0)){
+    stop("Please ensure hal-width errors are greater than or equal to zero")
+  }
+  else if (length(phi_basis) != nrow(beta_phi)){
+    stop("Please ensure the number of basis features phi_i equals the number of columns (beta_phi)_i = cov(phi_i, Z)")
+    }
   
   
   # Remove Z_i for which e_i \in B^{\perp}
@@ -72,7 +96,8 @@ BudgetIV <- function(
   tau_vec <- reduced_problem$tau_red
   m_vec <- reduced_problem$m_red
   
-  d_Z <- nrow(beta_y)
+  d_Z <- ncol(beta_y)
+  d_Phi <- ncol(beta_phi)
   
   # If there are as many constraints as instruments, don't include tau_{K+1} = infinity
   if(m_vec[length(m_vec)] == d_Z){
@@ -105,12 +130,58 @@ BudgetIV <- function(
   # There are d_Z! /( m_1!(m_2 - m_1)!...(m_{K_red}-m_{K_red - 1})! ) perms. 
   while (!is.null(curr_S_perm)) {
     
-    lower_bounds <- -taus[curr_S_perm]
-    upper_bounds <- taus[curr_S_perm]
+    #lower_bounds <- -taus[curr_S_perm] - delta_beta_y
+    # lower_bounds <- -taus[curr_S_perm] - delta_beta_y
+    bounds <- taus[curr_S_perm] + delta_beta_y
     
-    print(upper_bounds)
+    print(bounds)
     
-    print(lower_bounds)
+    # print(upper_bounds)
+    # 
+    # print(lower_bounds)
+    
+    # lpsolve for testing intersection:
+    
+    # print("beta_phi")
+    # print(beta_phi)
+    
+    f.con <- rbind(t(beta_phi), - t(beta_phi))  # Combine the upper and lower bound constraints
+    f.dir <- c(rep("<=", d_Z), rep("<=", d_Z))  # Directions of the inequalities
+    f.rhs <- c(beta_y + bounds, bounds - beta_y)  # Right-hand side for the inequalities
+    
+    f.obj <- rep(0, d_Phi)  # Objective function for theta (length m, the dimension of theta)
+    
+    # print("f.con")
+    # print(f.con)
+    # 
+    # print("f.dir")
+    # print(f.dir)
+    # 
+    # print("f.rhs")
+    # print(f.rhs)
+    
+    solution <- lp("min", f.obj, f.con, f.dir, f.rhs, scale=TRUE)
+    
+    print(solution)
+    
+    # solution <- Rglpk_solve_LP(obj = f.obj, mat = f.con, dir = f.dir, rhs = f.rhs, max = TRUE)
+    
+    # if (solution$status == 0){
+    #   
+    #   # print(beta_phi)
+    #   # print(beta_y)
+    #   # 
+    #   # # print(bounds)
+    #   # print(curr_S_perm)
+    #   print("Soluable")
+    #   
+    # }
+    # 
+    # else {
+    #   
+    #   print("Insoluble")
+    #   
+    # }
     
     curr_S_perm <- S_perm_iter$getnext()
     
@@ -135,7 +206,7 @@ reduce_dZ <- function(
   
   changed_flag <- FALSE
   
-  d_Z <- nrow(beta_y)
+  d_Z <- ncol(beta_y)
   K <- length(tau_vec)
   
   m_new <- m_vec
@@ -144,8 +215,11 @@ reduce_dZ <- function(
   
   for(instrument in 1:d_Z){
     
-    instrument_basis_vec <- matrix(0, ncol=d_Z, nrow=1)
+    instrument_basis_vec <- matrix(0, ncol=1, nrow=d_Z)
     instrument_basis_vec[instrument] <- 1
+    
+    # print(beta_phi)
+    # print(instrument_basis_vec)
     
     instrument_projection <- beta_phi %*% instrument_basis_vec
     
@@ -155,7 +229,7 @@ reduce_dZ <- function(
       
       for(k in 1:K){
         
-        if(beta_y[,instrument] <= tau_vec[k]){
+        if(beta_y[, instrument] <= tau_vec[k]){
           m_new[k] <- m_new[k] - 1}}
       
       }
@@ -178,8 +252,8 @@ reduce_dZ <- function(
   
   m_red <- m_new[to_remove_constraints == 0]
   tau_red <- tau_vec[to_remove_constraints == 0]
-  beta_y_red <- beta_y[to_remove_instruments == 0, , drop=FALSE]
-  beta_phi_red <- beta_phi[to_remove_instruments == 0, , drop=FALSE]
+  beta_y_red <- beta_y[, to_remove_instruments == 0 , drop=FALSE]
+  beta_phi_red <- beta_phi[, to_remove_instruments == 0, drop=FALSE]
   
   # print(beta_y_red)
   # print(beta_phi_red)
@@ -191,13 +265,25 @@ reduce_dZ <- function(
               "partially_identifiable" = partially_identifiable, "proven_infeasible" = proven_infeasible))
 }
 
-beta_phi_true <- matrix(c(2, -4), ncol=1, nrow=2)
-beta_y_true <- matrix(c(0, -3.9), ncol=1, nrow=2)
-tau_vec <- c(0.2,3)
-m_vec <- c(1,2)
-dom_ATE <- matrix(c(-2, 2), ncol=, nrow=)
 
-BudgetIV(beta_y_true, beta_phi_true, tau_vec, m_vec, dom_ATE)
+# beta_phi_true <- matrix(c(1.2, -0.4, 1, 0.6, 0.3, -0.9, -0.2, 0.45), ncol=4, nrow=2)
+# beta_y_true <- matrix(c(0, -3.9, 3, -2), ncol=4, nrow=1)
+# delta_beta_y <- c(0.1,0.2, 0.2, 0.15)
+
+beta_phi_true <- matrix(c(1.2, -0.4, 1, 0.6, 0.3, -0.9, -0.2, 0.45), ncol=4, nrow=2)
+beta_y_true <- matrix(c(-0.3, 0.5, 3.4, 3.25), ncol=4, nrow=1)
+delta_beta_y <- c(0.1,0.2, 0.2, 0.15)
+
+tau_vec <- c(0.2,3)
+m_vec <- c(2,4)
+dom_ATE <- matrix(c(-4, 4), ncol=1, nrow=2)
+search_res_ATE <- 500
+
+phi_basis <- expression(x, x^2)
+
+# print(length(phi_basis))
+
+BudgetIV(beta_y=beta_y_true, beta_phi=beta_phi_true, phi_basis=phi_basis, tau_vec=tau_vec, m_vec=m_vec, search_dom_ATE=dom_ATE, delta_beta_y=delta_beta_y)
 
 
 # deltas <- c(1,2)
